c37d727aa83dba2223289eaae3c3f953b4dd1c21
suri_local <surendrakumar_kothuru@apple.com>
2024-12-18 17:46:25 +0530

    Run Cucumber reports

--- src/test/java/cucumber/TestNgRunner.java
+++ src/test/java/cucumber/TestNgRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/cucumber",glue="SeleniumJavaFramework.stepImplementations",monochrome=true,
-tags="@ErrorValidation" ,plugin= {"html:target/cucumber.html"})
+tags="@Regression" ,plugin= {"html:target/cucumber.html"})
 public class TestNgRunner extends AbstractTestNGCucumberTests{
 
 }
